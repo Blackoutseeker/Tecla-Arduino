cc046445fb7d2f99d8486c09917361da2a23c699
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2020-11-05 18:49:01 -0300

    Create Botao (antigo).ino

---
+++ Botao (antigo).ino
@@ -0,0 +1,21 @@
+#include <Keyboard.h>
+
+// exemplo: quero a tecla "F5", será KEY_F5
+// para ter acesso a todas as teclas especias visite: https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardmodifiers/
+// altere a tecla abaixo conforme a necessidade, basta alterar a parte do "F"
+
+char tecla = KEY_F8;
+
+void setup() {
+  pinMode(7, INPUT_PULLUP);
+  Keyboard.begin();
+}
+
+void loop() {
+  if(digitalRead(7) == LOW) {
+    Keyboard.press(tecla);
+    delay(100);
+    Keyboard.release(tecla);
+    delay(800);
+  }
+}
